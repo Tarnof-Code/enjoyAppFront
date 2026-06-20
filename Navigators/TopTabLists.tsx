88dec35d302b53c3295a70dcb89ac9178fca9589
Tarnof <tarnof@hotmail.com>
2026-06-21 00:34:38 +0200

    Suppression des écrans de groupes en dur de l'onglet Listes

--- Navigators/TopTabLists.tsx
+++ Navigators/TopTabLists.tsx
@@ -2,16 +2,9 @@ import * as React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { Foundation } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
-import General from '../screens/Lists/General';
-import Crabs from '../screens/Lists/Crabs';
-import Sharks from '../screens/Lists/Sharks';
-import Octopuses from '../screens/Lists/Octopuses';
 import Animators from '../screens/Lists/Animators';
 import Bedrooms from '../screens/Lists/Bedrooms';
 import Header from '../Components/Header';
@@ -23,67 +16,11 @@ const Tab = createMaterialTopTabNavigator<ListsTabParamList>();
 function TopTab() {
   return (
     <Tab.Navigator
-      initialRouteName="General"
+      initialRouteName="Animators"
       screenOptions={{
         tabBarActiveTintColor: colors.ink,
       }}
     >
-      <Tab.Screen
-        name="General"
-        component={General}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <Foundation
-              size={25}
-              name="torsos-all"
-              color={focused ? colors.ink : colors.disabled}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Crabs"
-        component={Crabs}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <FontAwesome5
-              size={25}
-              name="pastafarianism"
-              color={focused ? colors.ink : colors.disabled}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Sharks"
-        component={Sharks}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <MaterialCommunityIcons
-              size={25}
-              name="shark-fin"
-              color={focused ? colors.ink : colors.disabled}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Octopuses"
-        component={Octopuses}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <FontAwesome5
-              size={25}
-              name="octopus-deploy"
-              color={focused ? colors.ink : colors.disabled}
-            />
-          ),
-        }}
-      />
-
       <Tab.Screen
         name="Animators"
         component={Animators}
--- Navigators/types.ts
+++ Navigators/types.ts
@@ -14,10 +14,6 @@ export type BottomTabParamList = {
 };
 
 export type ListsTabParamList = {
-  General: undefined;
-  Crabs: undefined;
-  Sharks: undefined;
-  Octopuses: undefined;
   Animators: undefined;
   Bedrooms: undefined;
 };
